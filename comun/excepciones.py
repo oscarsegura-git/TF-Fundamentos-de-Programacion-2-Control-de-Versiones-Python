63fc3b1daf0428d6177203668bad12f5945d4b0b
Oscar Segura <oscar.roger.segura.vasquez@gmail.com>
2026-07-14 11:00:17 -0500

    Add custom exception classes for error handling

---
+++ comun/excepciones.py
@@ -0,0 +1,29 @@
+class SGPError(Exception):
+    pass
+
+class DatoInvalidoError(SGPError):
+    pass
+
+class DniInvalidoError(DatoInvalidoError):
+    pass
+
+class ClienteDuplicadoError(SGPError):
+    pass
+
+class ClienteNoEncontradoError(SGPError):
+    pass
+
+class ProductoDuplicadoError(SGPError):
+    pass
+
+class ProductoNoEncontradoError(SGPError):
+    pass
+
+class PedidoNoEncontradoError(SGPError):
+    pass
+
+class TransicionInvalidaError(SGPError):
+    pass
+
+class EntregaNoDisponibleError(SGPError):
+    pass
